d8b89cfed68f4bcb72abe44aacd32dfe492c03d0
zhoujunpeng <zjp0432@163.com>
2016-07-18 15:44:07 +0800

    assets push

--- deploy.js
+++ deploy.js
@@ -6,4 +6,5 @@ require("shelljs/global");
 
 cp('-Rf',path.join(__dirname,'.tmp/js/*'),path.join(__dirname,'js'));
 cp('-Rf',path.join(__dirname,'.tmp/style/*'),path.join(__dirname,'css'));
+cp('-Rf',path.join(__dirname,'.tmp/assets/*'),path.join(__dirname,'assets'));
 rm('-rf',path.join(__dirname,'.tmp'))
